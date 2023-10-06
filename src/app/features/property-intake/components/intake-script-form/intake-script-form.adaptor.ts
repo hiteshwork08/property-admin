9472c60f3f013f6d5afddbf0edcf6cb1283e6295
Hiteshwork@08 <Hiteshwork08@gmali.com>
2023-10-06 18:02:20 +0530

    additonal design added

--- src/app/features/property-intake/components/intake-script-form/intake-script-form.adaptor.ts
+++ src/app/features/property-intake/components/intake-script-form/intake-script-form.adaptor.ts
@@ -1,69 +1,104 @@
-import { Injectable, inject } from "@angular/core";
-import { AbstractRequestFormAdaptor } from "@common/form/abstract-request-form.adaptor";
-import { ToastrService } from "ngx-toastr";
-import { Subject, of } from "rxjs";
-import { PropertIntakeFormEnum, PropertyIntakeModel } from "../../property-intake.model";
-import { FormGroup } from "@angular/forms";
-import { WrapFormControl } from "@common/form/abstract-form.adaptor";
+import { Injectable, inject } from '@angular/core';
+import { AbstractRequestFormAdaptor } from '@common/form/abstract-request-form.adaptor';
+import { ToastrService } from 'ngx-toastr';
+import { Subject, of } from 'rxjs';
+import {
+  PropertIntakeFormEnum,
+  PropertyIntakeModel,
+} from '../../property-intake.model';
+import { FormGroup } from '@angular/forms';
+import { WrapFormControl } from '@common/form/abstract-form.adaptor';
 
-export type Utilities = UtilitiesEnum.Electric | UtilitiesEnum.Gas | UtilitiesEnum.Water |UtilitiesEnum.Sewer| UtilitiesEnum.ElectricNearby | UtilitiesEnum.PowerPolesLinesNearby
+export type Utilities =
+  | UtilitiesEnum.Electric
+  | UtilitiesEnum.Gas
+  | UtilitiesEnum.Water
+  | UtilitiesEnum.Sewer
+  | UtilitiesEnum.ElectricNearby
+  | UtilitiesEnum.PowerPolesLinesNearby;
 
 export enum UtilitiesEnum {
-    Electric = 'Electric',
-    Gas = 'Gas',
-    Water ='Water',
-    Sewer = 'Sewer',
-    ElectricNearby = 'ElectricNearby',
-    PowerPolesLinesNearby= 'PowerPolesLinesNearby'
+  Electric = 'Electric',
+  Gas = 'Gas',
+  Water = 'Water',
+  Sewer = 'Sewer',
+  ElectricNearby = 'Electric Nearby',
+  PowerPolesLinesNearby = 'Power Poles Lines Nearby',
 }
 
-export const AllUtilities : Utilities[] = [UtilitiesEnum.Electric, UtilitiesEnum.Gas,UtilitiesEnum.Water ,UtilitiesEnum.Sewer, UtilitiesEnum.ElectricNearby , UtilitiesEnum.PowerPolesLinesNearby ]
+export enum IntakeScriptFormControlStates {
+  Yes,
+  No,
+  Not_sure,
+}
+
+export type IntakeScriptFormControlStateType1 =
+  | IntakeScriptFormControlStates.Yes
+  | IntakeScriptFormControlStates.No;
+export type IntakeScriptFormControlStateType2 =
+  | IntakeScriptFormControlStates.Yes
+  | IntakeScriptFormControlStates.No
+  | IntakeScriptFormControlStates.Not_sure;
+
+export const AllUtilities: Utilities[] = [
+  UtilitiesEnum.Electric,
+  UtilitiesEnum.Gas,
+  UtilitiesEnum.Water,
+  UtilitiesEnum.Sewer,
+  UtilitiesEnum.ElectricNearby,
+  UtilitiesEnum.PowerPolesLinesNearby,
+];
 
 export interface SubmitIntakeScriptOfferFormData {
-    ableToNotarize: boolean
-    ableToNotarizeNotes: string
-    haveDeedCopy: boolean
-    canScanDeed: boolean
-    yearsOwned: string
-    hasPOA: boolean
-    yearlyPOAFee: string
-    hasHOA: boolean
-    yearlyHOAFee: string
-    backTaxesOwed: string
-    hasLiens: boolean
-    hasLiensNotes: string
-    uniqueFeatures: string
-    hasEasement: boolean
-    hasEasementNotes: string
-    improvements: string
-    additionalProperties: string
-    inTakeScriptNotes: string
-    hasPictures: boolean
-    hasSurvey: boolean
-    utilities: Utilities[]
-    Comment:string
-  }
-
+  ableToNotarize: boolean;
+  ableToNotarizeNotes: string;
+  haveDeedCopy: boolean;
+  canScanDeed: boolean;
+  yearsOwned: string;
+  hasPOA: boolean;
+  yearlyPOAFee: string;
+  hasHOA: boolean;
+  yearlyHOAFee: string;
+  backTaxesOwed: string;
+  hasLiens: boolean;
+  hasLiensNotes: string;
+  uniqueFeatures: string;
+  hasEasement: boolean;
+  hasEasementNotes: string;
+  improvements: string;
+  additionalProperties: string;
+  inTakeScriptNotes: string;
+  hasPictures: boolean;
+  hasSurvey: boolean;
+  utilities: Utilities[];
+  Comment: string;
+}
 
 export interface SubmitIntakeScriptOfferRes {}
 
 @Injectable()
-export class SubmitIntakescriptOfferFormAdaptor extends AbstractRequestFormAdaptor<SubmitIntakeScriptOfferFormData, SubmitIntakeScriptOfferRes> {
-    override name = "submit-intake-script-offer";
-    private propertyIntakeModel = inject(PropertyIntakeModel);
-    private toastr = inject(ToastrService);
-    readonly formData$ = new Subject<SubmitIntakeScriptOfferFormData>();
-    override onRequest(formGroup: SubmitIntakeScriptOfferFormData) {
+export class SubmitIntakescriptOfferFormAdaptor extends AbstractRequestFormAdaptor<
+  SubmitIntakeScriptOfferFormData,
+  SubmitIntakeScriptOfferRes
+> {
+  override name = 'submit-intake-script-offer';
+  private propertyIntakeModel = inject(PropertyIntakeModel);
+  private toastr = inject(ToastrService);
+  readonly formData$ = new Subject<SubmitIntakeScriptOfferFormData>();
+  override onRequest(formGroup: SubmitIntakeScriptOfferFormData) {
+    this.propertyIntakeModel.propertyIntakeStatus =
+      PropertIntakeFormEnum.intakeApproval;
 
-      this.propertyIntakeModel.propertyIntakeStatus = PropertIntakeFormEnum.intakeApproval;
-  
-      console.log("Form saved...", formGroup);
-      this.toastr.success("Success!", "Record was saved successfully");
-      return of({});
-    }
-
-    override onSuccess(formData: SubmitIntakeScriptOfferFormData, res: SubmitIntakeScriptOfferRes, formGroup: FormGroup<WrapFormControl<SubmitIntakeScriptOfferFormData>>): void {
-      this.formData$.next(formData)
-    }
+    console.log('Form saved...', formGroup);
+    this.toastr.success('Success!', 'Record was saved successfully');
+    return of({});
   }
-  
+
+  override onSuccess(
+    formData: SubmitIntakeScriptOfferFormData,
+    res: SubmitIntakeScriptOfferRes,
+    formGroup: FormGroup<WrapFormControl<SubmitIntakeScriptOfferFormData>>
+  ): void {
+    this.formData$.next(formData);
+  }
+}
--- src/app/features/property-intake/components/intake-script-form/intake-script-form.component.ts
+++ src/app/features/property-intake/components/intake-script-form/intake-script-form.component.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/forms';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {
@@ -23,6 +23,9 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { tap } from 'rxjs';
 import {
   AllUtilities,
+  IntakeScriptFormControlStateType1,
+  IntakeScriptFormControlStateType2,
+  IntakeScriptFormControlStates,
   SubmitIntakeScriptOfferFormData,
   SubmitIntakescriptOfferFormAdaptor,
   Utilities,
@@ -31,6 +34,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { Data } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-intake-script-form',
@@ -40,6 +46,7 @@ import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
   imports: [
     ReactiveFormsModule,
     CommonModule,
+    MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     FormHandlerModule,
@@ -53,36 +60,40 @@ import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
     FormsModule,
     MatCheckboxModule,
     ReactiveFormsModule,
+    MatRadioModule,
   ],
   providers: [provideFormAdaptor(SubmitIntakescriptOfferFormAdaptor, true)],
 })
 export class IntakeScriptFormComponent {
   @Output() formData = new EventEmitter<SubmitIntakeScriptOfferFormData>();
 
+  IntakeScriptFormControlStates = IntakeScriptFormControlStates;
+
   form = new FormGroup({
-    ableToNotarize: new FormControl<boolean>(false),
+    ableToNotarize: new FormControl<IntakeScriptFormControlStateType1>(null),
     ableToNotarizeNotes: new FormControl<string>(null),
-    haveDeedCopy: new FormControl<boolean>(false),
-    canScanDeed: new FormControl<boolean>(false),
+    haveDeedCopy: new FormControl<IntakeScriptFormControlStateType1>(null),
+    canScanDeed: new FormControl<IntakeScriptFormControlStateType1>(null),
     yearsOwned: new FormControl<string>(null),
-    hasPOA: new FormControl<boolean>(false),
+    hasPOA: new FormControl<IntakeScriptFormControlStateType2>(null),
     yearlyPOAFee: new FormControl<string>({ value: null, disabled: true }),
-    hasHOA: new FormControl<boolean>(false),
+    hasHOA: new FormControl<IntakeScriptFormControlStateType2>(null),
     yearlyHOAFee: new FormControl<string>({ value: null, disabled: true }),
     backTaxesOwed: new FormControl<string>(null),
-    hasLiens: new FormControl<boolean>(false),
+    hasLiens: new FormControl<IntakeScriptFormControlStateType2>(null),
     hasLiensNotes: new FormControl<string>({ value: null, disabled: true }),
-    uniqueFeatures: new FormControl<string>(null),
-    hasEasement: new FormControl<boolean>(false),
+    uniqueFeatures: new FormControl<string[]>([]),
+    hasEasement: new FormControl<IntakeScriptFormControlStateType2>(null),
     hasEasementNotes: new FormControl<string>({ value: null, disabled: true }),
-    improvements: new FormControl<string>(null),
-    additionalProperties: new FormControl<string>(null),
+    improvements: new FormControl<string[]>([]),
+    additionalProperties: new FormControl<string[]>([]),
     inTakeScriptNotes: new FormControl<string>(null),
-    hasPictures: new FormControl<boolean>(false),
-    hasSurvey: new FormControl<boolean>(false),
+    hasPictures: new FormControl<IntakeScriptFormControlStateType1>(null),
+    hasSurvey: new FormControl<IntakeScriptFormControlStateType1>(null),
     utilities: new FormControl<Utilities[]>([]),
     Comment: new FormControl<string>(null),
   });
+  separatorKeysCodes: number[] = [ENTER, COMMA];
 
   constructor(
     private submitIntakescriptOfferFormAdaptor: SubmitIntakescriptOfferFormAdaptor
@@ -90,7 +101,8 @@ export class IntakeScriptFormComponent {
     this.form.controls.ableToNotarize.valueChanges
       .pipe(
         tap((data) => {
-          data
+          this.form.controls.ableToNotarizeNotes.reset();
+          data === IntakeScriptFormControlStates.No
             ? this.form.controls.ableToNotarizeNotes.disable()
             : this.form.controls.ableToNotarizeNotes.enable();
         })
@@ -99,7 +111,8 @@ export class IntakeScriptFormComponent {
     this.form.controls.hasPOA.valueChanges
       .pipe(
         tap((data) => {
-          data
+          data === IntakeScriptFormControlStates.Yes ||
+          IntakeScriptFormControlStates.Not_sure
             ? this.form.controls.yearlyPOAFee.enable()
             : this.form.controls.yearlyPOAFee.disable();
         })
@@ -108,7 +121,8 @@ export class IntakeScriptFormComponent {
     this.form.controls.hasHOA.valueChanges
       .pipe(
         tap((data) => {
-          data
+          data === IntakeScriptFormControlStates.Yes ||
+          IntakeScriptFormControlStates.Not_sure
             ? this.form.controls.yearlyHOAFee.enable()
             : this.form.controls.yearlyHOAFee.disable();
         })
@@ -117,7 +131,8 @@ export class IntakeScriptFormComponent {
     this.form.controls.hasLiens.valueChanges
       .pipe(
         tap((data) => {
-          data
+          data === IntakeScriptFormControlStates.Yes ||
+          IntakeScriptFormControlStates.Not_sure
             ? this.form.controls.hasLiensNotes.enable()
             : this.form.controls.hasLiensNotes.disable();
         })
@@ -126,7 +141,8 @@ export class IntakeScriptFormComponent {
     this.form.controls.hasEasement.valueChanges
       .pipe(
         tap((data) => {
-          data
+          data === IntakeScriptFormControlStates.Yes ||
+          IntakeScriptFormControlStates.Not_sure
             ? this.form.controls.hasEasementNotes.enable()
             : this.form.controls.hasEasementNotes.disable();
         })
@@ -139,4 +155,61 @@ export class IntakeScriptFormComponent {
   }
 
   utilities: Utilities[] = AllUtilities;
+
+  formControlChange(controls, val) {
+    this.form.controls[controls].setValue(val);
+  }
+
+  addFeature(event: MatChipInputEvent): void {
+    const uniqueFeatures = this.form.controls.uniqueFeatures.value;
+    const value = (event.value || '').trim();
+    if (value) {
+      uniqueFeatures.push(value);
+    }
+    event.chipInput!.clear();
+  }
+
+  removeFeature(feat: string): void {
+    const uniqueFeatures = this.form.controls.uniqueFeatures.value;
+    const index = uniqueFeatures.indexOf(feat);
+
+    if (index >= 0) {
+      uniqueFeatures.splice(index, 1);
+    }
+  }
+  addimprovements(event: MatChipInputEvent): void {
+    const improvements = this.form.controls.improvements.value;
+    const value = (event.value || '').trim();
+    if (value) {
+      improvements.push(value);
+    }
+    event.chipInput!.clear();
+  }
+
+  removeimprovements(fruit: string): void {
+    const improvements = this.form.controls.improvements.value;
+    const index = improvements.indexOf(fruit);
+
+    if (index >= 0) {
+      improvements.splice(index, 1);
+    }
+  }
+
+  addProperties(event: MatChipInputEvent): void {
+    const additionalProperties = this.form.controls.additionalProperties.value;
+    const value = (event.value || '').trim();
+    if (value) {
+      additionalProperties.push(value);
+    }
+    event.chipInput!.clear();
+  }
+
+  removeadditionalProperties(property: string): void {
+    const additionalProperties = this.form.controls.additionalProperties.value;
+    const index = additionalProperties.indexOf(property);
+
+    if (index >= 0) {
+      additionalProperties.splice(index, 1);
+    }
+  }
 }
